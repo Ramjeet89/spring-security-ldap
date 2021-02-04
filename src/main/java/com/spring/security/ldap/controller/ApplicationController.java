35c0e31ecf717c97eb1c4c04d9f742709d65886b
Ramjeet89 <ramjeet.mahto77@gmail.com>
2021-02-04 17:52:37 +0530

    implemented spring security with ldap application

---
+++ src/main/java/com/spring/security/ldap/controller/ApplicationController.java
@@ -0,0 +1,17 @@
+package com.spring.security.ldap.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class ApplicationController {
+
+    @GetMapping("/secure")
+    public String secureMethod() {
+        return "secure Rest end point";
+    }
+
+
+}
